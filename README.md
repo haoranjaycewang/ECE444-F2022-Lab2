73dc9c8e72a9232f98ffb8ee425e2feb642b30f6
unknown <haoranjayce.wang@mail.utoronto.ca>
2022-09-27 18:32:10 -0400

    Lab2 Activity4: Completed final commit for end of lab2 and final update of readme

--- README.md
+++ README.md
@@ -4,18 +4,40 @@ By: Jayce Wang
 This repo is a clone of https://github.com/miguelgrinberg/flasky
 
 
-**Activity 1**
+## **Activity 1**
 
 Note that I named my venv: "flask_venv"
 
 ![Activity 1 Screenshot 1](screenshots/lab2/Lab2_activity1_sc.PNG "Activity 1 Screenshot 1")
 
 
-**Activity 2**
+## **Activity 2**
 
 ![Activity 2 Screenshot 1](screenshots/lab2/Lab2_activity2_sc.PNG "Activity 2 Screenshot 1")
 
 
-**Activity 3**
+## **Activity 3**
 
 ![Activity 3 Screenshot 1](screenshots/lab2/Lab2_activity3_sc.PNG "Activity 3 Screenshot 1")
+
+
+## **Activity 4**
+
+Step 2 screenshot 1
+
+![Activity 4 Screenshot 1](screenshots/lab2/Lab2_activity4_sc1.PNG "Activity 4 Screenshot 1")
+
+
+Step 2 screenshot 2
+
+![Activity 4 Screenshot 2](screenshots/lab2/Lab2_activity4_sc2.PNG "Activity 4 Screenshot 2")
+
+
+Step 2 screenshot 3
+
+![Activity 4 Screenshot 3](screenshots/lab2/Lab2_activity4_sc3.PNG "Activity 4 Screenshot 3")
+
+
+Step 2 screenshot 4
+
+![Activity 4 Screenshot 4](screenshots/lab2/Lab2_activity4_sc4.PNG "Activity 4 Screenshot 4")
--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,Email
 
 
 app = Flask(__name__)
@@ -23,12 +23,21 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        if 'utoronto' in form.email.data:
+            session['UofT_email'] = True
+        else:
+            session['UofT_email'] = None
+        session['email'] = form.email.data
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html',form=form, name=session.get('name'))
+    return render_template('index.html',form=form, name=session.get('name'),email=session.get('email'),UofT_email=session.get('UofT_email'))
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?",validators=[DataRequired()])
+    email = StringField("What is your UofT Email address?",validators=[DataRequired(),Email()])
     submit = SubmitField("Submit")
